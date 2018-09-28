c4d25f3d0a1c1b62152ad22f8f0e2b408b87bb20
Sebastian Reinhard <sebastian@bember.com>
2018-09-28 13:22:53 +0200

    Populate CodeLens blocks from AST

--- src/providers/httpCodeLensProvider.ts
+++ src/providers/httpCodeLensProvider.ts
@@ -3,51 +3,25 @@
 import { CancellationToken, CodeLens, CodeLensProvider, Command, Range, TextDocument } from 'vscode';
 import * as Constants from '../common/constants';
 import { Selector } from '../utils/selector';
+import * as Ast from '../utils/ast';
 
 export class HttpCodeLensProvider implements CodeLensProvider {
-    public provideCodeLenses(document: TextDocument, token: CancellationToken): Promise<CodeLens[]> {
+    public async provideCodeLenses(document: TextDocument, token: CancellationToken): Promise<CodeLens[]> {
+        let ast = Ast.parse(document);
+
         let blocks: CodeLens[] = [];
-        let lines: string[] = document.getText().split(Constants.LineSplitterRegex);
-        let delimitedLines: number[] = Selector.getDelimiterRows(lines);
-        delimitedLines.push(lines.length);
 
-        let requestRange: [number, number][] = [];
-        let start: number = 0;
-        for (let index = 0; index < delimitedLines.length; index++) {
-            let end = delimitedLines[index] - 1;
-            if (start <= end) {
-                requestRange.push([start, end]);
-                start = delimitedLines[index] + 1;
-            }
-        }
-
-        for (let index = 0; index < requestRange.length; index++) {
-            let [blockStart, blockEnd] = requestRange[index];
-
-            // get real start for current requestRange
-            while (blockStart <= blockEnd) {
-                if (Selector.isEmptyLine(lines[blockStart]) ||
-                    Selector.isCommentLine(lines[blockStart]) ||
-                    Selector.isVariableDefinitionLine(lines[blockStart])) {
-                    blockStart++;
-                } else {
-                    break;
-                }
-            }
-
-            if (Selector.isResponseStatusLine(lines[blockStart])) {
+        for (let node of ast.children)
+        {
+            if (node.type != Ast.NodeType.Request)
                 continue;
-            }
 
-            if (blockStart <= blockEnd) {
-                const range = new Range(blockStart, 0, blockEnd, 0);
-                const cmd: Command = {
-                    arguments: [document, range],
-                    title: 'Send Request',
-                    command: 'rest-client.request'
-                };
-                blocks.push(new CodeLens(range, cmd));
-            }
+            const cmd: Command = {
+                arguments: [document, node.range],
+                title: 'Send Request',
+                command: 'rest-client.request'
+            };
+            blocks.push(new CodeLens(node.range, cmd));
         }
 
         return Promise.resolve(blocks);
