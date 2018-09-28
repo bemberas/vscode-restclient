a58d8965213d625d36a1c8eba5b88bbc33e76bf9
Sebastian Reinhard <sebastian@bember.com>
2018-09-28 10:11:15 +0200

    Use AST for resolving file variables
    This adds support for @@include, but disables caching for now.

--- src/utils/httpVariableProviders/fileVariableProvider.ts
+++ src/utils/httpVariableProviders/fileVariableProvider.ts
@@ -1,11 +1,12 @@
 'use strict';
 
-import { TextDocument } from 'vscode';
+import { TextDocument, Range, workspace } from 'vscode';
 import * as Constants from "../../common/constants";
 import { ResolveErrorMessage } from '../../models/httpVariableResolveResult';
 import { VariableType } from '../../models/variableType';
 import { calculateMD5Hash } from '../misc';
 import { HttpVariableProvider, HttpVariableValue } from './httpVariableProvider';
+import * as Ast from "../ast";
 
 export class FileVariableProvider implements HttpVariableProvider {
     private static _instance: FileVariableProvider;
@@ -23,10 +24,6 @@ export class FileVariableProvider implements HttpVariableProvider {
         ['r', '\r'],
         ['t', '\t']
     ]);
-
-    private readonly cache = new Map<string, HttpVariableValue[]>();
-
-    private readonly fileMD5Hash = new Map<string, string>();
 
     private constructor() {
     }
@@ -52,40 +49,33 @@ export class FileVariableProvider implements HttpVariableProvider {
         return this.getFileVariables(document);
     }
 
-    private async getFileVariables(document: TextDocument): Promise<HttpVariableValue[]> {
-        const file = document.uri.toString();
-        const fileContent = document.getText();
-        const fileHash = calculateMD5Hash(fileContent);
-        if (!this.cache.has(file) || fileHash !== this.fileMD5Hash.get(file)) {
-            const variables: HttpVariableValue[] = [];
-
-            const regex = new RegExp(Constants.FileVariableDefinitionRegex, 'mg');
-
-            let match: RegExpExecArray;
-            while (match = regex.exec(fileContent)) {
-                let key = match[1];
-                let originalValue = match[2];
-                let value = "";
-                let isPrevCharEscape = false;
-                for (const currentChar of originalValue) {
-                    if (isPrevCharEscape) {
-                        isPrevCharEscape = false;
-                        value += this.escapee.get(currentChar) || currentChar;
-                    } else {
-                        if (currentChar === "\\") {
-                            isPrevCharEscape = true;
-                            continue;
-                        }
-                        value += currentChar;
-                    }
-                }
-                variables.push({ name: key, value });
-            }
-
-            this.cache.set(file, variables);
-            this.fileMD5Hash.set(file, fileHash);
+    private async appendVariablesFromDocument(document: TextDocument, variables: Map<string, HttpVariableValue>) {
+        try {
+            var ast = Ast.parse(document);
         }
+        catch(ex)
+        {
+            debugger;
+        }
+        for (let node of ast.children) {
+            switch(node.type) {
+                case Ast.NodeType.Include: {
+                    let includeDocument = await workspace.openTextDocument(node.path.absolutePath);
+                    await this.appendVariablesFromDocument(includeDocument, variables);
+                    break;
+                }
+                case Ast.NodeType.FileVariable: {
+                    variables.set(node.key, { name: node.key, value: node.value });
+                    break;
+                }
+            }
+        }
+    }
 
-        return this.cache.get(file);
+    private async getFileVariables(document: TextDocument): Promise<HttpVariableValue[]> {
+        let variables = new Map<string, HttpVariableValue>();
+        await this.appendVariablesFromDocument(document, variables);
+        let values = [...variables.values()];
+        return values;
     }
 }
